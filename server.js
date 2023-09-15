39597b2aee027d1366963685deb930988e7ad93e
David Pirek <dpirek@gmail.com>
2023-09-15 11:28:47 -0400

    removing semicolumn

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ async function router(url) {
   if(url.pathname === '/') {
     const messages = await getMessages();
     return new Response(await renderComponent(Home, {messages}), {headers: headers})
-  };
+  }
   
   if(url.pathname === '/about') return new Response(await renderComponent(About), {headers: headers});
 }
