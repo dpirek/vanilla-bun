0167da969c4a4d427acd6bab1095bfa1f113fc1f
David Pirek <dpirek@gmail.com>
2023-09-19 00:58:59 -0400

    formating server time

--- components/Time.jsx
+++ components/Time.jsx
@@ -1,7 +1,11 @@
+function formatTime(date) {
+  return date.toTimeString().slice(0, 8);
+}
+
 function Time() {
   const date = new Date();
   return <div>
-    The time is: <strong>{date.toTimeString()}</strong>
+    The server time is: <strong>{formatTime(date)}</strong>
   </div>;
 }
 
