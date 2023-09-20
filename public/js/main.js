d9c4d4509bd4c7cdd22dce3c32c88aee4413a26e
David Pirek <dpirek@gmail.com>
2023-09-19 22:34:42 -0400

    ws connection refactor

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,17 @@ import ws from "./ws.js";
 
 const messageCointainer = document.querySelector('#message');
 
-// ws.onopen = (event) => {};
-// ws.onclose = (event) => {};
+ws.onopen = (event) => {
+  messageCointainer.innerHTML = `<div>
+  <strong style="color: green">...ws connected</strong>
+</div>`;
+};
+ws.onclose = (event) => {
+  messageCointainer.innerHTML = `<div>
+    <strong style="color: red">...lost ws connection</strong>
+  </div>`;
+};
 ws.onerror = (error) => console.error(error);
 ws.onmessage = (event) => {
-  if(messageCointainer) {
-    messageCointainer.innerHTML = event.data;
-  }
+  if(messageCointainer) messageCointainer.innerHTML = event.data;
 };
-
-console.log(ws);
